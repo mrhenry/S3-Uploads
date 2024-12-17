c2b5e81c5941a3a62092229be4cc1f8d1b8e8b6a
Romain Menke <romainmenke@gmail.com>
2024-12-17 17:58:34 +0100

    inline requires

--- s3-uploads.php
+++ s3-uploads.php
@@ -8,31 +8,39 @@ Version: 1.1.0
 Author URI: http://hmn.md
 */
 
-if ( defined( 'WP_CLI' ) && WP_CLI ) {
-	require_once dirname( __FILE__ ) . '/inc/class-s3-uploads-wp-cli-command.php';
+if (defined('WP_CLI') && WP_CLI ) {
+    include_once dirname(__FILE__) . '/inc/class-s3-uploads-wp-cli-command.php';
 }
 
-add_action( 'plugins_loaded', 's3_uploads_init' );
+require_once dirname(__FILE__) . '/inc/class-s3-uploads-changed-files-iterator.php';
+require_once dirname(__FILE__) . '/inc/class-s3-uploads-image-editor-imagick.php';
+require_once dirname(__FILE__) . '/inc/class-s3-uploads-local-stream-wrapper.php';
+require_once dirname(__FILE__) . '/inc/class-s3-uploads-stream-wrapper.php';
+require_once dirname(__FILE__) . '/inc/class-s3-uploads-uploadsyncbuilder.php';
+require_once dirname(__FILE__) . '/inc/class-s3-uploads.php';
 
-function s3_uploads_init() {
-	if ( ! s3_uploads_check_requirements() ) {
-		return;
-	}
+add_action('plugins_loaded', 's3_uploads_init');
 
-	if ( ! defined( 'S3_UPLOADS_BUCKET' ) ) {
-		return;
-	}
+function s3_uploads_init()
+{
+    if (! s3_uploads_check_requirements() ) {
+        return;
+    }
 
-	if ( ( ! defined( 'S3_UPLOADS_KEY' ) || ! defined( 'S3_UPLOADS_SECRET' ) ) && ! defined( 'S3_UPLOADS_USE_INSTANCE_PROFILE' ) ) {
-		return;
-	}
+    if (! defined('S3_UPLOADS_BUCKET') ) {
+        return;
+    }
 
-	if ( ! s3_uploads_enabled() ) {
-		return;
-	}
+    if (( ! defined('S3_UPLOADS_KEY') || ! defined('S3_UPLOADS_SECRET') ) && ! defined('S3_UPLOADS_USE_INSTANCE_PROFILE') ) {
+        return;
+    }
 
-	$instance = S3_Uploads::get_instance();
-	$instance->setup();
+    if (! s3_uploads_enabled() ) {
+        return;
+    }
+
+    $instance = S3_Uploads::get_instance();
+    $instance->setup();
 }
 
 /**
@@ -40,16 +48,17 @@ function s3_uploads_init() {
  *
  * @return bool True if the requirements are met, else false.
  */
-function s3_uploads_check_requirements() {
-	if ( version_compare( '5.3.3', PHP_VERSION, '>' ) ) {
-		if ( is_admin() && ! defined( 'DOING_AJAX' ) ) {
-			add_action( 'admin_notices', 's3_uploads_outdated_php_version_notice' );
-		}
+function s3_uploads_check_requirements()
+{
+    if (version_compare('5.3.3', PHP_VERSION, '>') ) {
+        if (is_admin() && ! defined('DOING_AJAX') ) {
+            add_action('admin_notices', 's3_uploads_outdated_php_version_notice');
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	return true;
+    return true;
 }
 
 /**
@@ -57,10 +66,12 @@ function s3_uploads_check_requirements() {
  *
  * This has to be a named function for compatibility with PHP 5.2.
  */
-function s3_uploads_outdated_php_version_notice() {
-	printf( '<div class="error"><p>The S3 Uploads plugin requires PHP version 5.3.3 or higher. Your server is running PHP version %s.</p></div>',
-		PHP_VERSION
-	);
+function s3_uploads_outdated_php_version_notice()
+{
+    printf(
+        '<div class="error"><p>The S3 Uploads plugin requires PHP version 5.3.3 or higher. Your server is running PHP version %s.</p></div>',
+        PHP_VERSION
+    );
 }
 
 /**
@@ -71,39 +82,17 @@ function s3_uploads_outdated_php_version_notice() {
  *
  * @return bool
  */
-function s3_uploads_enabled() {
-	// Make sure the plugin is enabled when autoenable is on
-	$constant_autoenable_off = ( defined( 'S3_UPLOADS_AUTOENABLE' ) && false === S3_UPLOADS_AUTOENABLE );
+function s3_uploads_enabled()
+{
+    // Make sure the plugin is enabled when autoenable is on
+    $constant_autoenable_off = ( defined('S3_UPLOADS_AUTOENABLE') && false === S3_UPLOADS_AUTOENABLE );
 
-	if ( $constant_autoenable_off && 'enabled' !== get_option( 's3_uploads_enabled' ) ) {                         // If the plugin is not enabled, skip
-		return false;
-	}
+    if ($constant_autoenable_off && 'enabled' !== get_option('s3_uploads_enabled') ) {                         // If the plugin is not enabled, skip
+        return false;
+    }
 
-	return true;
+    return true;
 }
-
-/**
- * Autoload callback.
- *
- * @param $class_name Name of the class to load.
- */
-function s3_uploads_autoload( $class_name ) {
-	/*
-	 * Load plugin classes:
-	 * - Class name: S3_Uploads_Image_Editor_Imagick.
-	 * - File name: class-s3-uploads-image-editor-imagick.php.
-	 */
-	$class_file = 'class-' . strtolower( str_replace( '_', '-', $class_name ) ) . '.php';
-	$class_path = dirname( __FILE__ ) . '/inc/' . $class_file;
-
-	if ( file_exists( $class_path ) ) {
-		require $class_path;
-
-		return;
-	}
-}
-
-spl_autoload_register( 's3_uploads_autoload' );
 
 // Require AWS Autoloader file.
-require_once dirname( __FILE__ ) . '/lib/aws-sdk/aws-autoloader.php';
+require_once dirname(__FILE__) . '/lib/aws-sdk/aws-autoloader.php';
