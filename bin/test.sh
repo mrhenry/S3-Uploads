945c42c9015dd819f6da86150f4d9c9902531a0d
Romain Menke <romainmenke@gmail.com>
2026-01-14 13:19:20 +0100

    wip

--- bin/test.sh
+++ bin/test.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+curl \
+	-X POST --insecure --data '{ "name": "test-bucket", "location": "US", "storageClass": "STANDARD", "iamConfiguration": { "uniformBucketLevelAccess": { "enabled": true } } }' \
+	-H "Content-Type: application/json" \
+	"https://0.0.0.0:4443/storage/v1/b"
+
 S3_UPLOADS_BASE_URL="http://127.0.0.1:9090" S3_UPLOADS_BUCKET=test-bucket S3_UPLOADS_KEY=1234567890 S3_UPLOADS_SECRET=valid-secret S3_UPLOADS_BUCKET_URL="http://127.0.0.1:9090" S3_UPLOADS_REGION="" WP_DEVELOP_DIR=./wp-test-root/wordpress-tests-lib ./vendor/bin/phpunit;
